8c4b381e5e1dd4e38d07c3e342a23c92e8d76f8f
mallasudi <Suniti.malla08@gmail.com>
2026-05-06 10:16:13 +0545

    feature 13 updated

--- Frontend/src/Pages/customer/Profile.jsx
+++ Frontend/src/Pages/customer/Profile.jsx
@@ -1,11 +1,5 @@
-import React from 'react';
-
 function Profile() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Profile</h1>
-    </div>
-  );
+  return <h1>Profile Details</h1>;
 }
 
 export default Profile;
